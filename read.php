361443a79c31620248af037086000ee946b9c9ea
Steff Brouwer <sbrouwer05@gmail.com>
2023-01-10 11:53:36 +0100

    connecten met db 1

---
+++ read.php
@@ -0,0 +1,16 @@
+<?php
+
+include('config.php');
+
+$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=UTF8";
+
+try {
+    $pdo = new PDO($dsn, $dbUser, $dbPass);
+    if ($pdo) {
+        echo "Er is een verbinding met de mysql server";
+    } else {
+        echo "Er is een interne server error opgetreden"; 
+    }
+} catch(PDOException $e) {
+    echo $e->getMessage();
+}
