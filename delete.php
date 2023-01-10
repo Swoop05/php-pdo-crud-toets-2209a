03a02ea442a3801f52122a8fb6a173413f158894
Steff Brouwer <sbrouwer05@gmail.com>
2023-01-10 12:14:29 +0100

    delete deel 2

--- delete.php
+++ delete.php
@@ -15,3 +15,19 @@ try {
 } catch (PDOException $e) {
     $e->getMessage();
 }
+
+$sql = "DELETE FROM DureAuto
+        WHERE Id = :Id;";
+
+if ($sql) {
+    echo "het record is verwijderd";
+    header('Refresh:1.5; read.php');
+} else {
+    echo " het bracket is niet verwijderd";
+}
+
+$statement = $pdo->prepare($sql);
+
+$statement->bindValue(':Id', $_GET['Id'], PDO::PARAM_INT);
+
+$result = $statement->execute();
